92e24e422ee6dd7220ecc1ec03f2bbcbc75f8023
David Corticchiato <dawi@drone>
2013-10-29 14:30:51 +0100

    enhance yat test

--- test/yat.test.js
+++ test/yat.test.js
@@ -10,15 +10,31 @@ var content = {
     {"el": "button"},
     {"el": "label"},
     {"el": "textfield"},
-    {"el": "view"}
+    {
+      "el": "view",
+      "egg": "spam",
+      "eggegg": "spamspam"
+    }
   ]
 };
 
 
-exports['test tree#length'] = function(beforeExit, assert) {
+exports['test tree length'] = function(beforeExit, assert) {
   var yat = wyatt.yat(content);
 
   assert.equal(2, yat.where({el: 'view'}).length);
+};
+
+exports['test queries'] = function(beforeExit, assert) {
+  var yat = wyatt.yat(content);
+
+  assert.equal('spamspam', yat.first({el: 'view', egg: 'spam'}).eggegg);
+};
+
+exports['test generated element type'] = function(beforeExit, assert) {
+  var yat = wyatt.yat(content);
+
+  assert.equal(true, yat.first({el: 'view'}) instanceof wyatt.el('view'));
 };
 
 exports['test subqueries'] = function(beforeExit, assert) {
