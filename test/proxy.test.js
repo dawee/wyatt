5b2eb565143708eec931630acfdcd97be305f53e
David Corticchiato <dawi@drone>
2013-11-04 13:45:00 +0100

    manages add of a yat document

--- test/proxy.test.js
+++ test/proxy.test.js
@@ -2,6 +2,7 @@ var Ti = require('titanium-namespace');
 var assert = require("assert");
 var logger = require('../lib/logger');
 var Proxy = require('../lib/proxy');
+var wyatt = require('..');
 
 Proxy._setter = 'patch';
 
@@ -21,6 +22,14 @@ describe('Proxy', function () {
     proxy.ui = Ti.UI.createView();
     proxy.append({el: 'view'})
     assert.equal(1, proxy.where().length);
+  });
+
+  it('should manages dynamics append of a yat document', function () {
+    var proxy = new Proxy();
+
+    proxy.ui = Ti.UI.createView();
+    proxy.append(wyatt.yat({el: 'view', tree: [{el: 'view'}]}));
+    assert.equal(2, proxy.where().length);
   });
 
 });
